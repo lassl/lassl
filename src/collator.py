f8335e685f919ad3d9f5a846d82c4cc853a46c14
Minho Ryu <ryumin93@gmail.com>
2021-12-02 10:25:41 +0900

    [python] feat: Add bert processor (#29)
    * [python] feat: Add bert processor
    - Bert 프로세서 추가
    - Bert Collator 추가: WWM + SOP
    
    Refs: #28
    
    * [python] refactor: revise bert collator name
    - DataCollatorForBert -> DataCollatorForBertWithSOP
    
    Refs: #28
    
    * [python] refactor: make code more clean
    - 사용되지 않는 chinese_ref 제거
    - examples에 대해서 for 문이 2번 사용되는 비효율 제거
    
    Refs: #28
    
    * [python] refactor: revise name of prepare method
    - prepare_sop_from_examples -> prepare_wwm_and_sop_from_examples
    
    Refs: #28

--- src/collator.py
+++ src/collator.py
@@ -1,8 +1,68 @@
 import random
 from typing import Any, Dict, List, Optional
 
-from transformers import DataCollatorForLanguageModeling
+from transformers import DataCollatorForLanguageModeling, DataCollatorForWholeWordMask
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.data.data_collator import _torch_collate_batch
+
+
+def tolist(x):
+    if isinstance(x, list):
+        return x
+    elif hasattr(x, "numpy"):  # Checks for TF tensors without needing the import
+        x = x.numpy()
+    return x.tolist()
+
+
+class DataCollatorForBertWithSOP(DataCollatorForWholeWordMask):
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizerBase,
+        mlm_probability: float = 0.15,
+        pad_to_multiple_of: Optional[int] = None,
+    ):
+        self.tokenizer = tokenizer
+        self.mlm_probability = mlm_probability
+        self.pad_to_multiple_of = pad_to_multiple_of
+    
+    def __call__(self, examples: List[Dict[str, Any]]) -> Dict[str, Any]:
+        examples = self.prepare_wwm_and_sop_from_examples(examples)
+        batch = self.tokenizer.pad(examples, return_tensors="pt", pad_to_multiple_of=self.pad_to_multiple_of)
+        batch_mask = batch.pop("mask_label")
+        batch["input_ids"], batch["labels"] = self.torch_mask_tokens(batch["input_ids"], batch_mask)
+        return batch
+
+    def prepare_wwm_and_sop_from_examples(self, examples: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        output_examples = []
+        for example in examples:
+            chunk_ids = example["input_ids"]
+            seq_length = len(chunk_ids)
+            start, end = seq_length // 3, seq_length // 3 * 2
+            split_position = random.randrange(start, end)
+            reverse = random.random() < 0.5
+
+            if reverse:
+                token_a = chunk_ids[split_position:]
+                token_b = chunk_ids[:split_position]
+            else:
+                token_a = chunk_ids[:split_position]
+                token_b = chunk_ids[split_position:]
+
+            input_ids = self.tokenizer.build_inputs_with_special_tokens(token_a, token_b)
+            token_type_ids = self.tokenizer.create_token_type_ids_from_sequences(token_a, token_b)
+            sentence_order_label = 1 if reverse else 0
+            ref_tokens = self.tokenizer.convert_ids_to_tokens(input_ids)
+            mask_label = self._whole_word_mask(ref_tokens)
+
+            output_examples.append(
+                {
+                    "input_ids": input_ids,
+                    "token_type_ids": token_type_ids,
+                    "next_sentence_label": sentence_order_label,
+                    "mask_label": mask_label,
+                }
+            )
+        return output_examples
 
 
 class DataCollatorForSOP(DataCollatorForLanguageModeling):
