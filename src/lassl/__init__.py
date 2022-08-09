f87b638dde8c72bab22cc2e0e129459fb6a5b33c
DaehanKim <kdh5852@gmail.com>
2022-08-09 06:44:42 +0000

    ul2 init

--- src/lassl/__init__.py
+++ src/lassl/__init__.py
@@ -5,7 +5,8 @@ from lassl.collators import (
     DataCollatorForBert,
     DataCollatorForGpt2,
     DataCollatorForRoberta,
-    DataCollatorForT5
+    DataCollatorForT5,
+    DataCollatorForUL2
 )
 from lassl.processors import (
     AlbertProcessor,
@@ -13,7 +14,8 @@ from lassl.processors import (
     BertProcessor,
     GPT2Processor,
     RobertaProcessor,
-    T5Processor
+    T5Processor,
+    UL2Processor
 )
 
 # in train_tokenizer.py
@@ -23,7 +25,8 @@ MODEL_TYPE_TO_PREDEFINED_MODEL = {
     "roberta": "roberta-base",
     "albert": "albert-base-v2",
     "bart": "facebook/bart-base",
-    "t5" : "t5-small"
+    "t5" : "t5-small",
+    "ul2" : "t5-base"
 }
 
 # in serialize_corpora.py
@@ -33,7 +36,8 @@ MODEL_TYPE_TO_PROCESSOR = {
     "gpt2": GPT2Processor,
     "albert": AlbertProcessor,
     "bart": BartProcessor,
-    "t5" : T5Processor
+    "t5" : T5Processor,
+    "ul2" : UL2Processor
 }
 
 # in pretrain_language_model.py
@@ -43,5 +47,6 @@ MODEL_TYPE_TO_COLLATOR = {
     "roberta": DataCollatorForRoberta,
     "gpt2": DataCollatorForGpt2,
     "bart": DataCollatorForBart,
-    "t5" : DataCollatorForT5
+    "t5" : DataCollatorForT5,
+    "ul2" : DataCollatorForUL2
 }
